6a05161306ea6c5eb79cfac316cdb2d151f1fa31
NathaelB <kirua48000@gmail.com>
2022-07-28 09:19:51 +0200

    :construction: add `Navbar`, `Header` & `First Part`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
 import type { NextPage } from 'next'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <div className="text-gray-700">
-      Hello World
+
+      <Navbar />
+      <div className="content">
+        <Header />
+        <div className="content-wrapper min-h-[100vh]">
+          <div className="separator">
+            <div className="separator--container">
+              <h3
+                className="absolute top-0 text-white mx-auto w-full text-center h-full items-center justify-center flex">exploration
+                and adventure</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
     </div>
   )
 }

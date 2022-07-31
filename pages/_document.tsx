fdbaa2b594029355b21d68114e550fbc31a8212a
NathaelB <kirua48000@gmail.com>
2022-07-31 02:12:47 +0200

    :construction: document for SEO

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+const Document = () => {
+  return (
+    <Html lang="fr">
+      <Head>
+        <title>Hypolia</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document

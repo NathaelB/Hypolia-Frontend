4ce177ddb1a5a2ce7d619df376a41247d3689d65
NathaelB <kirua48000@gmail.com>
2022-07-31 02:10:24 +0200

    :construction: add Abeille

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,27 @@
 import Image from 'next/image'
 import LogoText from '../public/logo.png'
+import Abeille from './Abeille'
+
 const Header = () => {
   return (
     <div className="min-h-[100vh] relative header">
 
       <div className="home-cover h-screen">
-        <div className="text-white">
+        <Abeille className={"top-1/2 abeille1"}/>
+        <div className="hidden lg:block">
+          <Abeille className={"top-1/3 abeille2"} />
+          <Abeille className={"top-1/2 left-1/3 abeille3"} />
+          <Abeille className={"top-2/3 right-10 abeille4"} />
+        </div>
+
+        <div className="">
           <div className=" mx-auto h-screen flex flex-col gap-4  place-content-center -translate-y-20">
-            <span className="text-white font-thin font-serif text-7xl self-center">Rejoins-nous sur</span>
-            <div className="self-center w-1/3">
+            <span className="text-white font-thin font-serif text-3xl lg:text-7xl self-center">Rejoins-nous sur</span>
+            <div className="self-center w-2/3 lg:w-1/3">
               <Image src={LogoText} alt={"Logo"} />
             </div>
           </div>
-
-
         </div>
-
       </div>
     </div>
   )

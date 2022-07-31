7ff62560619dca72acfef9f5b55261b5cc2b662e
NathaelB <kirua48000@gmail.com>
2022-07-31 02:12:18 +0200

    :construction: start design UI

---
+++ pages/authentication/login.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from 'next'
+
+const Login: NextPage = () => {
+  return (
+    <div className="login">
+      <div className="video">
+        <video loop playsInline autoPlay>
+          <source src="https://d2vkoy1na2a6o6.cloudfront.net/videos/fsg-bg-wide-1d7a662b187e51758a166d75380b8e7bcb62ce97ddfd50f0eb0aefef32b27f478fbe293ea6cfff31f8b5e6b1ed2b5fbe1c0e0ca48bbf0408ddf6d4bc0172073b.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div className="section--panel--decoration">
+        <div className="decoration--left z-20">
+          <span/>
+        </div>
+        <div className="decoration--right z-20">
+          <span/>
+        </div>
+      </div>
+      <div className="content">
+test
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ pages/who-we-are/index.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Hypolia - Qui sommes-nous</title>
+      </Head>
+      <div>
+        Hello Who we are
+      </div>
+    </>
+  )
+}
+
+export default Home
